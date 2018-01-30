45476b772392d5bff17d5a5b15a11b726ee54c03
sarunaskvietka <sarunas.kvietka@gmail.com>
2018-01-30 15:54:22 +0200

    komentaras

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,40 +5,19 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("iveskite skaiciu");
+        System.out.println("iveskite svori");
         Scanner sc = new Scanner(System.in);
-        int sk1 = sc.nextInt();
-        System.out.println("iveskite antra skaiciu");
-        int sk2 = sc.nextInt();
-        int suma = sk1 + sk2;
-        int skirtumas = sk1 - sk2;
-        System.out.println("suma=" + suma(sk1, sk2));
-        System.out.println("suma=" + suma ((float)sk1, (float) sk2));
-        System.out.println("skirtumas=" + skirtumas);
-        System.out.println("skirtumas=" + skirtumas);
-
-    }
-
-    public static int suma(int sk1, int sk2) {
-        return sk1 + sk2;
-    }
-
-    public static float suma(float sk1, float sk2) {
-        return sk1 + sk2;
-    }
-
-    ;
-
-    public static int skirtumas(int sk1, int sk2) {
-        return sk1 - sk2;
-    }
-
-    public static float skirtumas(float sk1, float sk2) {
-        return sk1 - sk2;
-    }
-
-    {
-
-
+        float svoris = sc.nextInt();
+        System.out.println("iveskite ugi");
+        float ugis = sc.nextFloat();
+        float Kmi = svoris / (ugis * ugis);
+        System.out.println("jusu indeksas" + Kmi);
     }
 }
+
+
+
+
+
+
+
