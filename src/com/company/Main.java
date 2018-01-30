463e2b33d2e5b91c87b0b74fae93217f6912c43f
sarunaskvietka <sarunas.kvietka@gmail.com>
2018-01-30 15:21:20 +0200

    komentaras

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,10 +5,16 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("iveskite varda");
-        Scanner sc = new Scanner (System.in);
-        String vardas = sc.nextLine();
-        System.out.println("iveskite varda" + vardas.length());
+        System.out.println("iveskite skaiciu");
+        Scanner sc = new Scanner(System.in);
+        int sk1 = sc.nextInt();
+        System.out.println("iveskite antra skaiciu");
+        int sk2 = sc.nextInt();
+        int suma = sk1 + sk2;
+        int skirtumas = sk1 - sk2;
+                System.out.println("suma=" + suma);
+                System.out.println("skirtumas="+skirtumas);
+
 
     }
 }
